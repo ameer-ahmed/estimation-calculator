12a5c806a8d404be8264ccf638812f54895e09f1
Ameer Ahmed <ameer2001an@gmail.com>
2021-09-05 00:29:28 +0200

    Initial commit.

---
+++ android/app/src/main/kotlin/com/example/estimation_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.estimation_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(EstimationCalculatorApp());
+}
+
+class EstimationCalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Where we start.
+    return Center(
+      child: Text('Where we start!'),
+    );
+  }
+}
